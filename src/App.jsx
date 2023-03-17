60d2a45e7ddda6977faeb6773fdec9193919cee4
IhzhaNB <ihzhanauvalb@gmail.com>
2023-03-17 23:28:50 +0700

    router commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import Portofolio from "./pages/Portofolio";
+// import FormValidate from "./pages/FormValidate";
+// import NewsLC from "./pages/NewsLC";
+// import HookNews from "./pages/HookNews";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import Home from "./pages/Home";
+
+function App() {
+  return (
+    <>
+      <Portofolio />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/Portofolio/components/work/Data.jsx
@@ -0,0 +1,33 @@
+import Work1 from "../../assets//formvalidation.jpeg";
+import Work2 from "../../assets//lifecyclenews.jpeg";
+import Work3 from "../../assets//hooknews.jpeg";
+
+export const projectsData = [
+  {
+    id: 1,
+    image: Work1,
+    title: "Form Validation",
+    url: "https://validation-form-kappa.vercel.app/",
+    category: "My Task",
+  },
+  {
+    id: 2,
+    image: Work2,
+    title: "Lifecycle News",
+    url: "https://lifecycle-news-eta.vercel.app/",
+    category: "My Task",
+  },
+  {
+    id: 3,
+    image: Work3,
+    title: "Hook News",
+    url: "https://hook-news.vercel.app/",
+    category: "My Task",
+  },
+];
+
+export const projectsNav = [
+  {
+    name: "My Task",
+  },
+];
